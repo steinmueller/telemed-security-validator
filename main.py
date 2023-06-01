d7fff98f89cef20946b295b277f93196b2fc8f48
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-01 13:03:57 +0200

    init

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def startpage():
+    return "<h1>Hello, World!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
