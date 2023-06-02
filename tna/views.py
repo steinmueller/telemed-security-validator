bcc6aac8b2b6f7c74a382e861046a3e4297b81db
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-02 12:11:50 +0200

    add render for all templates

--- tna/views.py
+++ tna/views.py
@@ -1,8 +1,16 @@
 #Routes for the Website
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+def landingpage():
+    return render_template("landingpage.html")
+
+@views.route('/home')
 def home():
-    return '/html/startpage.html'
+    return render_template("home.html")
+
+@views.route('/app')
+def app():
+    return render_template("app.html")
