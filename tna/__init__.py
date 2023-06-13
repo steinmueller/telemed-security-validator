eb0fc9c8e08f08fa673e9021071b473ddc55b1da
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-13 18:12:40 +0200

    add login

--- tna/__init__.py
+++ tna/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask, url_for
+#from keycloak import KeycloakOpenID
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
-    #sp = TnaServiceProvider()
-
     app = Flask(__name__)
 
     # TODO Never share - Is there any more security measure necessary???
     app.config['SECRET_KEY'] = 'abnolreizug890a349ghil23r847t90p8grhfaio'
+    # TODO adapt to azure DB
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # import blueprints
     from .views import views
@@ -16,6 +24,22 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    
+    from .models import User, Mission
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
+
+def create_database(app):
+    if not path.exists('tna/' + DB_NAME):
+        db.create_all(app = app)
+        print('Database created')
