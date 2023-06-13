679fa3e5348451ab1a0608dbcb9036659668b0c1
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-13 18:13:11 +0200

    update for login

--- tna/views.py
+++ tna/views.py
@@ -1,16 +1,20 @@
 #Routes for the Website
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def landingpage():
-    return render_template("landingpage.html")
+    return render_template("landingpage.html", user=current_user)
 
+# TODO add functionalities for the web app
 @views.route('/home')
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
 
 @views.route('/app')
+@login_required
 def app():
-    return render_template("app.html")
+    return render_template("app.html", user=current_user)
