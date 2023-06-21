acb873f53ad17fd61bf5643a46151b5e3880e9fc
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-21 08:49:06 +0200

    add oidc

--- tna/views.py
+++ tna/views.py
@@ -1,7 +1,12 @@
 #Routes for the Website
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import get_app
 
+app = get_app()
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
@@ -10,11 +15,11 @@ def landingpage():
 
 # TODO add functionalities for the web app
 @views.route('/home')
-@login_required
+#@oidc.require_login
 def home():
     return render_template("home.html", user=current_user)
 
 @views.route('/app')
-@login_required
+#@oidc.require_login
 def app():
     return render_template("app.html", user=current_user)
