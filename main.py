651cb9d074dfc7eba8aa359ad615f7a7f19ac9de
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-21 08:47:13 +0200

    add views

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #import from __init__
-from tna import create_app
+from tna import get_app, views, auth
 
-app = create_app()
+app = get_app()
+
+from tna.views import views
+from tna.auth import auth # TODO DAS HIER IST DAS PROBLEM!!!
+
+# register blueprints
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
 
 if __name__ == "__main__":
     app.run(debug=True)
