00bd3dbf5d7246254b580535aa69da052188965c
Sebastian Steinmüller <ga94dey@mytum.de>
2023-06-21 08:47:48 +0200

    add keycloak envs

--- tna/__init__.py
+++ tna/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, g, session
 #from keycloak import KeycloakOpenID
 from flask_sqlalchemy import SQLAlchemy
 from os import path
@@ -10,19 +10,21 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
 
-    # TODO Never share - Is there any more security measure necessary???
-    app.config['SECRET_KEY'] = 'abnolreizug890a349ghil23r847t90p8grhfaio'
-    # TODO adapt to azure DB
+    app.config.from_envvar('KEYCLOAK_FLASK_SETTINGS')
+
+    # # TODO Never share - Is there any more security measure necessary???
+    # app.config['SECRET_KEY'] = 'abnolreizug890a349ghil23r847t90p8grhfaio'
+    # # TODO adapt to azure DB
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # #app.config['OIDC_CLIENT_SECRETS'] = 'client_secrets.json'
+    # app.config['OIDC_ID_TOKEN_COOKIE_SECURE'] = False
+    # app.config['OIDC_REQUIRED_VERIFIED_EMAIL'] = False
+    # app.config['OIDC_USER_INFO_ENABLED'] = True
+    # app.config['OIDC_OPENID_REALM'] = 'tna-auth'
+    # app.config['OIDC_SCOPES'] = ['openid', 'email', 'profile']
+    # app.config['OIDC_INTROSPECTION_AUTH_METHOD'] = 'client_secret_post'
+
     db.init_app(app)
-
-    # import blueprints
-    from .views import views
-    from .auth import auth
-
-    # register blueprints
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Mission
 
@@ -39,7 +41,17 @@ def create_app():
 
     return app
 
-def create_database(app):
-    if not path.exists('tna/' + DB_NAME):
-        db.create_all(app = app)
-        print('Database created')
+app = create_app()
+
+def get_app():
+    return app
+
+@app.before_request
+def load_user():
+    g.username = session.get('username')
+    g.access_token = session.get('access_token')
+
+# def create_database(app):
+#     if not path.exists('tna/' + DB_NAME):
+#         db.create_all(app = app)
+#         print('Database created')
